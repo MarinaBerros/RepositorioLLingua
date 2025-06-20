02f8fd0369dd86cddbf45695c3fde73adb3be8a3
MarinaBerros <eva.proyectoalgodon.com>
2025-06-20 11:47:46 +0200

    cambios en bloque 22

--- templates.js
+++ templates.js
@@ -3,8 +3,8 @@ function cargarHeader() {
     return `
         <header>
         <div class="logo-titulo">
-            <a href="index.html" class="logo-link">
-                <img src="img/logo-llingua.png" alt="Logo Llingua Alluga">
+            <a href="../index.html" class="logo-link">
+                <img src="../img/logo-llingua.png" alt="Logo Llingua Alluga">
                 <h1>LA LLINGUA ALLUGA</h1>
             </a>
         </div>
@@ -26,10 +26,34 @@ function cargarHeader() {
             <div id="suggestions" class="suggestions-box"></div>
         </div>
         </header>
-       
-        <nav class="menu-principal">
-            <!-- ... tu menú actual ... -->
-        </nav>
+         <nav class="menu-principal">
+        <div class="menu-item">
+                <button class="menu-btn">SOBRE EL PROYECTO</button>
+                <div class="submenu">
+                    <a href="materiales/autoria.html">Autoria</a>
+                    <a href="#">TFG</a>
+                </div>
+            </div>
+            
+            <div class="menu-item">
+                <button class="menu-btn">BLOQUES</button>
+                <div class="submenu">
+                    <a href="bloques/bloque1.html">Bloque 1: Comunicación oral</a>
+                    <a href="bloques/bloque2.html">Bloque 2: Comunicación escrita</a>
+                    <a href="bloques/bloque3.html">Bloque 3: Dialectología</a>
+                    <a href="bloques/bloque4.html">Bloque 4: Educación lliteraria</a>
+                </div>
+            </div>
+            
+            <div class="menu-item">
+                <button class="menu-btn">RECURSOS DIGITALES</button>
+                <div class="submenu">
+                    <a href="#">Vídeos</a>
+                    <a href="#">Otros blogs</a>
+                    <a href="#">Diccionarios</a>
+                </div>
+            </div>
+                </nav>
     `;
 }
 
